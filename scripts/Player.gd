f189c7ed9bb18877b6035f61d3b31697786ffaa3
Reik0z <demian.hott@ug.uchile.cl>
2022-04-02 18:18:49 -0300

    CamaraV2

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -14,15 +14,13 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	var posicionJefe = Vector2(jefe.global_transform.origin.x,jefe.global_transform.origin.z)
-	var posicionPlayer = Vector2(global_transform.origin.x,global_transform.origin.z)
-	var anguloTargetP = posicionPlayer.angle_to(posicionJefe)
-	targetCamera.rotation.y = anguloTargetP
+	var puntoMirar = jefe.global_transform.origin
+	puntoMirar.y = global_transform.origin.y
+	targetCamera.look_at(puntoMirar, Vector3.UP)
 	var input = Vector3(Input.get_axis("izquierda","derecha"),0,Input.get_axis("arriba","abajo"))
 	velocidad = input.normalized()*rapidez+Vector3(0,rapidezY,0)
 	rapidezY -= gravedad*delta
 	if Input.is_action_pressed("salto"):
 		rapidezY = salto
-	velocidad = velocidad.rotated(Vector3.UP,anguloTargetP)
+	velocidad = velocidad.rotated(Vector3.UP,targetCamera.rotation.y)
 	velocidad = move_and_slide(velocidad)
-
