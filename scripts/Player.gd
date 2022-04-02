1ffcbfbd35ffb204620c732bd4cc6c31149842bb
unknown <b.contreras.2@ug.uchile.cl>
2022-04-02 17:39:51 -0300

    camara implementada

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,17 +1,28 @@
 extends KinematicBody
 
 var velocidad = Vector3(0,0,0)
-const rapidez = 10
-const salto = 30
-const gravedad = 300
+export var rapidez = 15
+export var salto = 30
+export var gravedad = 200
+onready var jefe = get_parent().get_node("Enemigo")
+onready var targetCamera = $TargetPlayer
+export var offsetC:Vector3 = Vector3(0,6,15)
+onready var rotarCamara = $TargetPlayer/Camera 
+var rapidezY = 0
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	var input = Vector3(Input.get_axis("ui_left","ui_right"),0,Input.get_axis("ui_up","ui_down"))
-	velocidad = velocidad.move_toward(input.normalized()*rapidez,0.8)
+	var posicionJefe = Vector2(jefe.global_transform.origin.x,jefe.global_transform.origin.z)
+	var posicionPlayer = Vector2(global_transform.origin.x,global_transform.origin.z)
+	var anguloTargetP = posicionPlayer.angle_to(posicionJefe)
+	targetCamera.rotation.y = anguloTargetP
+	var input = Vector3(Input.get_axis("izquierda","derecha"),0,Input.get_axis("arriba","abajo"))
+	velocidad = input.normalized()*rapidez+Vector3(0,rapidezY,0)
+	rapidezY -= gravedad*delta
+	if Input.is_action_pressed("salto"):
+		rapidezY = salto
+	velocidad = velocidad.rotated(Vector3.UP,anguloTargetP)
 	velocidad = move_and_slide(velocidad)
-	velocidad = velocidad.move_toward(Vector3(0,-gravedad,0),3)
-	if Input.is_action_pressed("ui_accept"):
-		velocidad.y = salto
+
