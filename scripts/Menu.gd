b4fa041d9bee40955e6d4214dd3fd13176228690
Demian Hott <demian.hott@ug.uchile.cl>
2022-06-04 20:00:22 -0400

    Se agregan cosas al menu

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,14 +1,34 @@
 extends MarginContainer
 
-onready var start=$PanelContainer/VBoxContainer/Start
-onready var exit=$PanelContainer/VBoxContainer/Exit
+var seleccionando = 0
 
-func _ready():
-	start.connect("pressed", self, "_on_start_pressed")
-	exit.connect("pressed", self, "_on_exit_pressed")
-	
+func _input(event):
+	if Input.is_action_pressed("abajo"):
+		seleccionando += 1
+		$katana.rect_position.y += 30
+	if Input.is_action_pressed("arriba"):
+		seleccionando -= 1
+		$katana.rect_position.y -= 30
+	# Cuando ya se selecciono
+	if Input.is_action_just_pressed("ataque"):
+		if seleccionando == 1:
+			#yield($AnimationPlayer, "animation_finished")
+			_on_start_pressed()
+		if seleccionando == 2:
+			#yield($AnimationPlater, "animation_finished")
+			_on_exit_pressed()
+	# Pararse en una opción
+	if seleccionando == 1:
+		$PanelContainer/VBoxContainer/Start.modulate = Color.purple
+		$PanelContainer/VBoxContainer/Exit.modulate = Color.white
+	elif seleccionando == 2:
+		$PanelContainer/VBoxContainer/Start.modulate = Color.white
+		$PanelContainer/VBoxContainer/Exit.modulate = Color.purple
+	elif seleccionando != 1 and seleccionando != 2:
+		$PanelContainer/VBoxContainer/Start.modulate = Color.white
+		$PanelContainer/VBoxContainer/Exit.modulate = Color.white
 func _on_start_pressed():
-	get_tree().change_scene("res://Main.tscn")
+	get_tree().change_scene("res://scenes/Main.tscn")
 	
 func _on_exit_pressed():
 	get_tree().quit()
