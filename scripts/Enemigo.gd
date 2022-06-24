6fc1427c11d87567a0a62c660bf31f90eac606ef
DiegoOpazoD <diegoopazod@gmail.com>
2022-06-24 11:58:11 -0400

    Animaciones
    Agregamos animaciones, faltan las definitivas

--- scripts/Enemigo.gd
+++ scripts/Enemigo.gd
@@ -27,5 +27,6 @@ func _physics_process(delta):
 	move_and_slide(Vector3(1,0,0) * rapidez, Vector3.UP)
 	if vida <= 0:
 		get_tree().change_scene("res://scenes/Final.tscn")
+	if Globales.enritmo: #
 
 
