574eb208245b989d87ff4c47fc95b226efff8593
Demian Hott <demian.hott@ug.uchile.cl>
2022-07-16 14:40:53 -0400

    se agrega cambio de escena despues de la muerte del jefe

--- scripts/Enemigo.gd
+++ scripts/Enemigo.gd
@@ -10,7 +10,7 @@ onready var vision = $RootNode
 onready var angulo = $angulo
 
 export var vidaMax = 40
-var vida = vidaMax				# vida del enemigo
+var vida = vidaMax					# vida del enemigo
 onready var malla = $MeshInstance
 #export var fov = 90             	# mitad del fov del enemigo
 export var izq = 270
@@ -29,15 +29,15 @@ onready var Animacion = $RootNode/AnimationPlayer/AnimationTree.get("parameters/
 onready var anim_tree = $RootNode/AnimationPlayer/AnimationTree
 const combatState = "c"				# estado de combate 
 const rifleState = "r"				# estado de rifle
-var State = combatState  				# le dice que estado tiene, combate or rifle
-const normalState = ""						# caminar lento	animación
-const rapidoState = "_f"						# caminar rápido animación
-var speedState = normalState					# le dice si caminar rápido o lento en la animación, es nulo para animación normal y rápido para 
+var State = combatState  			# le dice que estado tiene, combate or rifle
+const normalState = ""				# caminar lento	animación
+const rapidoState = "_f"			# caminar rápido animación
+var speedState = normalState		# le dice si caminar rápido o lento en la animación, es nulo para animación normal y rápido para
 
 
 
-func rotateEnemy(Derecha:bool):					# ayuda con la animación de rotación, derecha=true, izquierda=false
-	rotation.y += -(PI/2) if Derecha else +(PI/2)						# radianes 	
+func rotateEnemy(Derecha:bool):		# ayuda con la animación de rotación, derecha=true, izquierda=false
+	rotation.y += -(PI/2) if Derecha else +(PI/2)	# radianes
 	if vida > 0:
 		puedeCaminar = true
 
@@ -61,8 +61,7 @@ func take_damage():
 	if vida <= 0:
 		puedeCaminar = false
 		Animacion.travel(State+"_death")
-		#get_tree().change_scene("res://scenes/Final.tscn")
-		
+
 	print(vida)
 
 	if vida <= vidaMax/4:
@@ -72,30 +71,22 @@ func take_damage():
 		
 		
 func _physics_process(delta):
-	
-	
-	var puntoMirar = jugador.global_transform.origin		# con global transform se obtiene "el 0,0,0 del jefe" y .origin dice la posción en el mundo
+
+	var puntoMirar = jugador.global_transform.origin	# con global transform se obtiene "el 0,0,0 del jefe" y .origin dice la posción en el mundo
 	puntoMirar.y = global_transform.origin.y			# coordenada y de la pos en el mundo	
-	#look_at(puntoMirar, Vector3.UP)
 
 	angulo.look_at(puntoMirar,Vector3.UP)
-		
+
 	var anguloP = angulo.rotation_degrees.y			# ángulo del player respecto al frente del enemigo 
 	var anguloP360 = int(anguloP+180)				# ángulo del player entre 0 y 360
-#	print(anguloP360)
-	
+
 	if (anguloP360>225 and anguloP360<315): 		# grados
 		Animacion.travel(State+"_rotate_left")
 		puedeCaminar = false
 
-
 	if (anguloP360<135 and anguloP360 > 45):
 		Animacion.travel(State+"_rotate_right")
 		puedeCaminar = false
-		
-	
-
-
 
 	distanciaV = puntoMirar - global_transform.origin
 	distancia = distanciaV.length()
@@ -103,9 +94,6 @@ func _physics_process(delta):
 	 
 	if distancia <7:
 		Animacion.travel("c_punch_left")
-#	print(distancia)
-	
-	#print(direccion)
 
 	if !puedeCaminar:
 		return
@@ -115,7 +103,6 @@ func _physics_process(delta):
 		Animacion.travel(State+"_walk"+speedState)
 	else:
 		Animacion.travel(State+"_idle")
-		
 
 func _on_hitbox_c_punch_right_body_entered(body):
 	body.take_damage(10)
@@ -129,50 +116,5 @@ func _on_hitbox_c_punch_down_body_entered(body):
 func _on_hitbox_c_down_body_entered(body):
 	body.take_damage(20)
 
-
-
-
-			
-
-		
-		
-
-
-		
-		
-
-
-
-
-
-
-
-
-
-		
-		
-
-	
-	
-	
-	
-	
-	
-
-	
-	
-	
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
+func change_scene():
+	get_tree().change_scene("res://scenes/Final.tscn")
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,40 +6,31 @@ var vida = vidaMax
 
 export (float) var eps = 0.1
 
-export var rapidezW = 15								  # rapidez normal
-export var rapidezD = 60								  # rapidez de dasheo
-export var salto = 60                                     # valor de magnitud del salto
-export var gravedad = 200                                 # valor de magnitude de la gravedad
-onready var jefe = get_parent().get_node("Enemigo")       # se obtiene el nodo "Enemigo"
-onready var targetCamera = $TargetPlayer				  # se obtiene el nodo "TargetPlayer"
-onready var mesh = $TargetPlayer/MeshInstance	  		  # se obtiene el nodo "Player"
-export var offsetC = Vector3(0,6,15)			  		  # offset entre la cámara y el jugador 
-onready var rotarCamara = $TargetPlayer/Camera 			  # se obtiene el nodo "Camera"
-var rapidez = rapidezW                                    # rapidez del personaje
-var velocidad = Vector3(0,0,0) 			# vector velocidad, inicialmente en 0,0,0                           # se crea el vector velocidad del personaje
-var rapidezY = 0										  # rapidez inicial en Y
+export var rapidezW = 15								# rapidez normal
+export var rapidezD = 60								# rapidez de dasheo
+export var salto = 60									# valor de magnitud del salto
+export var gravedad = 200								# valor de magnitude de la gravedad
+onready var jefe = get_parent().get_node("Enemigo")		# se obtiene el nodo "Enemigo"
+onready var targetCamera = $TargetPlayer				# se obtiene el nodo "TargetPlayer"
+onready var mesh = $TargetPlayer/MeshInstance			# se obtiene el nodo "Player"
+export var offsetC = Vector3(0,6,15)					# offset entre la cámara y el jugador 
+onready var rotarCamara = $TargetPlayer/Camera 			# se obtiene el nodo "Camera"
+var rapidez = rapidezW									# rapidez del personaje
+var velocidad = Vector3(0,0,0) 							# vector velocidad, inicialmente en 0,0,0                           # se crea el vector velocidad del personaje
+var rapidezY = 0										# rapidez inicial en Y
 
 onready var dash_particles = $Particles
 
-# duracion del buffer
-export (int) var timerAtq
-
-# si estamos caminando hacia adelante
+export (int) var timerAtq								# duracion del buffer
 var adelante = false
-# si estamos caminando hacia atrás
 var atras = false
-# si estamos caminado hacia la derecha
 var derecha = false
-# si estamos caminado hcia la izquierda
 var izquierda = false
-# si estamos atacando
-export (bool) var atacando = false
-# si puede atacar
-var puede_atacar = true
+export (bool) var atacando = false						# si estamos atacando
+var puede_atacar = true									# si puede atacar
 var combo = 0
 var arr_ataques = ["new_slash_right", "new_slash_left", "new_kick", "finisher"]
-# si te estan pegando
-var pegando = false
+var pegando = false										# si te estan pegando
 
 # maquina de estado del animation tree
 onready var Animacion = $TargetPlayer/RootNode/AnimationPlayer/AnimationTree.get("parameters/playback")
@@ -69,31 +60,32 @@ func _ready():
 	colision_ref.disabled = true
 	atacando = false
 
-func _physics_process(delta):			# delta es 1/60 seg.+
+func _physics_process(delta):							# delta es 1/60 seg.+
 	if vida <= 0:
 		take_damage(1)
 		return
 	counter +=1
 
 	# fixed camara
-	var distCentro = global_transform.origin.distance_to(Vector3.ZERO)	#distancia al jefe
-	rotarCamara.translation.z = dist_max_camara-clamp((distCentro-dist_corte)/(dist_limite-dist_corte), 0, 1)*(dist_max_camara-dist_min_camara)
-	
-	var puntoMirar = jefe.global_transform.origin		# con global transform se obtiene "el 0,0,0 del jefe" y .origin dice la posción en el mundo
-	puntoMirar.y = global_transform.origin.y			# coordenada y de la pos en el mundo	
+	var distCentro = global_transform.origin.distance_to(Vector3.ZERO)			#distancia al jefe
+	rotarCamara.translation.z = dist_max_camara - clamp((distCentro - dist_corte)/(dist_limite - dist_corte), 0, 1)*(dist_max_camara - dist_min_camara)
+
+	var puntoMirar = jefe.global_transform.origin
+	puntoMirar.y = global_transform.origin.y
 	targetCamera.look_at(puntoMirar, Vector3.UP)
-	var input = Vector3(Input.get_axis("izquierda","derecha"),0,Input.get_axis("arriba","abajo"))			# recibe los inputs de movimineto 
+
+	var input = Vector3(Input.get_axis("izquierda","derecha"),0,Input.get_axis("arriba","abajo"))
 	adelante = input.z < 0
 	atras = input.z > 0  
 	derecha = input.x > 0 and !adelante
-	velocidad = input.normalized()*rapidez+Vector3(0,rapidezY,0)	# velocidad es el vector input normalizado por la rapidez más el vector de salto
-	if not is_on_floor():			# si no está en el piso
-		rapidezY -= gravedad*delta			# se aplica la gravedad
-	if Input.is_action_pressed("salto"):	# si se apreta la tecla de salto
-		if is_on_floor():					# si está en el piso salta, si no, no hace nada
-			take_damage(75)
-			print(vida)
-			rapidezY = salto				# salta
+
+	# salto
+	velocidad = input.normalized()*rapidez + Vector3(0,rapidezY,0)
+	if not is_on_floor():
+		rapidezY -= gravedad*delta
+	if Input.is_action_pressed("salto"):
+		if is_on_floor():
+			rapidezY = salto
 
 	if timerAtq > 0:
 		timerAtq -= 1
@@ -109,13 +101,12 @@ func _physics_process(delta):			# delta es 1/60 seg.+
 		rapidez = rapidezW
 		dashing = false
 		dashE = 0
-		
-	
+
 #	colision_ref.disabled = not atacando
 
 	velocidad = velocidad.rotated(Vector3.UP,targetCamera.rotation.y)
 	velocidad = move_and_slide(velocidad,Vector3.UP)
-	
+
 	# Logica de transiciones
 	if pegando:
 		return
@@ -172,7 +163,6 @@ func _input(event):
 			timerAtq = atck_cooldown
 		else:
 			atacando = true
-#			timerAtq = 120
 			Animacion.travel(arr_ataques[0])
 			combo = 1
 			anim_tree["parameters/conditions/transition_combo"] = false
