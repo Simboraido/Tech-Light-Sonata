223aba796573901b16e9f9e565b34392598b91bb
unknown <b.contreras.2@ug.uchile.cl>
2022-07-20 20:18:57 -0400

    mecanicas de gato

--- scripts/Texto.gd
+++ scripts/Texto.gd
@@ -1,7 +1,7 @@
 extends Control
 export (Array, String, MULTILINE) var dialogos = []
-var indice 	= 0					# indice del texto que se está leyendo
-onready var texto = $NinePatchRect/RichTextLabel			# donde se ve el texto
+var indice 	= 0										# indice del texto que se está leyendo
+onready var texto = $Texto/RichTextLabel			# donde se ve el texto
 
 func _ready():
 	texto.parse_bbcode(dialogos[0])
